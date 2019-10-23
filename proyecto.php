f0495997a7097015217eee74b42b3ed5ffb96653
EALucero <56940163+EALucero@users.noreply.github.com>
2019-10-23 19:06:15 -0300

    Add files via upload

---
+++ proyecto.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+  if (condition) {
+    # code...
+  }else {
+    # code...
+  }
+
+  if (condition) {
+  # code...
+  }
+   ?>
+
+  </body>
+</html>
