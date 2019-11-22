efaa6e3798f2b9c7eef5ebbc6f938609251c698f
EALucero <56940163+EALucero@users.noreply.github.com>
2019-11-22 01:08:39 -0300

    Update resultados.php

--- resultados.php
+++ resultados.php
@@ -38,7 +38,7 @@
       if ($soc==7) {
           if ($amb==0) {
               if ($rad==7) {
-              }header ("https://ealucero.github.io/Gusanito/alemania.html");
+              }href ("https://ealucero.github.io/Gusanito/alemania.html");
               }
             }
           }
