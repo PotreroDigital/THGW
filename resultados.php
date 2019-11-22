e8898b68baacac6ddf155eeecacce1e81927680a
EALucero <56940163+EALucero@users.noreply.github.com>
2019-11-22 01:27:40 -0300

    Update resultados.php

--- resultados.php
+++ resultados.php
@@ -38,7 +38,7 @@
       if ($soc==7) {
           if ($amb==0) {
               if ($rad==7) {
-              }header ("Location: http://www.ealucero.github.io/Gusanito/alemania.html");
+              }header ("Location: https://www.ealucero.github.io/Gusanito/alemania.html");
               }
             }
           }
